c6a59452f3b457bc2a89a780e3b8ac1ba8590aa6
Nora Morsi <morsin@uw.edu>
2021-05-25 10:42:14 -0700

    typo fix!

--- ProjectName/pages/motivationSlider.js
+++ ProjectName/pages/motivationSlider.js
@@ -6,7 +6,7 @@ import Slider from '@react-native-community/slider';
 export default function MotivationSlider (props) {
 
     const descriptions = {
-        1 : "I will put in the effor where it's easy.",
+        1 : "I will put in the effort where it's easy.",
         2 : "I am willing to make changes, even if it takes adjusting.",
         3 : "I will do whatever it takes!",
     }
