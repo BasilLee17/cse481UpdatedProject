1b35f3ac471b3c7946c7b488f9fc8d7d2b0df23e
yadiw <yadiw@uw.edu>
2021-05-28 03:29:13 +0800

    just realized that the confirm buttons arent working; changed to modals

--- ProjectName/pages/deleteItems.js
+++ ProjectName/pages/deleteItems.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { StyleSheet, Pressable, Text, View, Button } from 'react-native';
+import { StyleSheet, Pressable, Text, View, Modal } from 'react-native';
 import { baseProps } from 'react-native-gesture-handler/lib/typescript/handlers/gestureHandlers';
 
 export default function DeleteItems({ buttonContent, deleteItemsHandler }) {
 
-    const [confirm, setConfirm] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false);
+
 
     const styles = StyleSheet.create({
         button: {
@@ -31,22 +32,74 @@ export default function DeleteItems({ buttonContent, deleteItemsHandler }) {
             letterSpacing: 0.25,
             color: 'white',
           },
+          modalView: {
+            margin: 20,
+            backgroundColor: "white",
+            borderRadius: 20,
+            padding: 35,
+            alignItems: "center",
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 0,
+              height: 2
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 4,
+            elevation: 5
+          },
+          textStyle: {
+            color: "white",
+            fontWeight: "bold",
+            textAlign: "center"
+          },
+          modalText: {
+            marginBottom: 15,
+            textAlign: "center"
+          }
     });
 
         return (
+            <View>
+            <Modal
+                animationType='fade'
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>Are you sure you want to {buttonContent.toLowerCase()}?</Text>
+                        <View style={{ flexDirection: "row" }}> 
+                        <Pressable
+                            style={[styles.button, {backgroundColor: 'red'}]}
+                            onPress={() => { 
+                                setModalVisible(!modalVisible);
+                                deleteItemsHandler();
+                            }
+                            }
+                            >
+                            <Text style={styles.textStyle}>Confirm</Text>
+                        </Pressable>
+                        <Pressable
+                            style={[styles.button,  {backgroundColor: 'green'}]}
+                            onPress={() => setModalVisible(!modalVisible)}
+                            >
+                            <Text style={styles.textStyle}>Cancel</Text>
+                        </Pressable>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
             <Pressable 
-                style={[styles.button, 
-                confirm ?
-                {backgroundColor: 'red'} 
-                :   {backgroundColor:'#28965A',}]} 
+                style={[styles.button, modalVisible? {backgroundColor: 'gray'}:{backgroundColor: '#28965A'}]} 
                 onPress={() =>{
-                if (confirm) {
-                    deleteItemsHandler();
-                }
-                setConfirm(!confirm);
+                    setModalVisible(!modalVisible);
                 }}>
-                <Text style={styles.text}>{confirm ? "Confirm" : buttonContent }</Text>
+                <Text style={styles.text}>{ buttonContent }</Text>
             </Pressable>
+            </View>
 
         )
     
